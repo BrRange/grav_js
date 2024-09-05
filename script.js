9a21b41e5de8056877e998b568db00a805bda8b1
BurstRange <113287175+BrRange@users.noreply.github.com>
2024-09-05 17:52:49 -0300

    Bouncy

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+const body = document.querySelector("body")
+const keys = {}
+moving.clipping = false
+moving.loc = [0, 0]
+moving.speed = [0, 0]
+let controlMode = false
+everyOther = true;
 function getCenter(obj) {
     return [obj.loc[0] + obj.offsetWidth / 2, obj.loc[1] + obj.offsetHeight / 2]
 }
+function getVector(node1, node2) {
+    center1 = getCenter(node1);
+    center2 = getCenter(node2);
+    return [center2[0] - center1[0], center2[1] - center1[1]];
+}
 function getDist(node1, node2) {
-    return Math.sqrt((getCenter(node1)[0] - getCenter(node2)[0]) ** 2 + (getCenter(node1)[1] - getCenter(node2)[1]) ** 2)
+    vector = getVector(node1, node2);
+    return Math.sqrt(vector[0] ** 2 + vector[1] ** 2)
 }
 function getAngle(source, target) {
-    let angle = Math.atan2(getCenter(target)[1] - getCenter(source)[1], getCenter(target)[0] - getCenter(source)[0])
-    return [Math.cos(angle), Math.sin(angle)]
+    mag = getDist(source, target);
+    if(mag == 0) mag = 1;
+    vector = getVector(source, target)
+    return [vector[0] / mag, vector[1] / mag];
 }
 function offsetRNG(limits) {
     return Math.random() * (limits * 2 + 1) - limits
 }
 function move(obj, x, y) {
-    obj.speed[0] += x * (1 / obj.weight)
-    obj.speed[1] += y * (1 / obj.weight)
+    obj.speed[0] += x
+    obj.speed[1] += y
     obj.loc[0] += obj.speed[0]
     obj.loc[1] += obj.speed[1]
 }
@@ -22,16 +37,19 @@ function refresh(obj) {
     obj.style.top = obj.loc[1] + "px"
 }
 function collide(node1, node2, bounce) {
-    dist = getDist(node1, node2)
+    dist = getDist(node1, node2);
+    angle = getAngle(node2, node1);
     if (dist < node1.offsetWidth / 2 + node2.offsetWidth / 2) {
         if (!node1.clipping) {
-            node1.speed[0] = getAngle(node2, node1)[0] * Math.abs(node1.speed[0]) * bounce
-            node1.speed[1] = getAngle(node2, node1)[1] * Math.abs(node1.speed[1]) * bounce
+            dotP = node1.speed[0] * angle[0] + node1.speed[1] * angle[1];
+            node1.speed[0] -= 2 * dotP * angle[0] * bounce + 0.02 * node1.speed[0];
+            node1.speed[1] -= 2 * dotP * angle[1] * bounce + 0.02 * node1.speed[1];
             node1.clipping = true
         } else {
-            node1.loc[0] += (node1.offsetWidth / 2 + node2.offsetWidth / 2 - dist) * getAngle(node2, node1)[0]
-            node1.loc[1] += (node1.offsetHeight / 2 + node2.offsetHeight / 2 - dist) * getAngle(node2, node1)[1]
-            node1.clipping = false }
+            node1.loc[0] += (node1.offsetWidth / 2 + node2.offsetWidth / 2 - dist) * angle[0]
+            node1.loc[1] += (node1.offsetHeight / 2 + node2.offsetHeight / 2 - dist) * angle[1]
+            node1.clipping = false
+        }
     } else {
         node1.clipping = false
     }
@@ -40,7 +58,7 @@ let fixeds = []
 let fixed = null
 for (i = 0; i < 10; i++) {
     fixeds[i] = document.createElement("section")
-    document.querySelector("body").appendChild(fixeds[i])
+    body.appendChild(fixeds[i])
     fixeds[i].style = "background-color: #0F0; width: 50vh; height: 50vh; border: #844 solid 10pt; position: absolute; border-radius: 100%;"
     fixeds[i].weight = 10
     fixeds[i].speed = [0, 0]
@@ -55,44 +73,43 @@ function getClosest(obj) {
     }
     return num[0]
 }
-const keys = {}
-moving.clipping = false
-moving.loc = [0, 0]
-moving.speed = [0, 0]
-moving.weight = 1
-let controlMode = false
-everyOther = true;
 function render(focusObj) {
-    camOffset = [getCenter(focusObj)[0] - window.innerWidth / 2, getCenter(focusObj)[1] - window.innerHeight / 2]
-    for (c in document.querySelector("body").childNodes) {
+    focusCenter = getCenter(focusObj);
+    camOffset = [focusCenter[0] - window.innerWidth / 2, focusCenter[1] - window.innerHeight / 2]
+    for (c in body.childNodes) {
         try {
-            document.querySelector("body").childNodes[c].loc[0] -= camOffset[0]
-            document.querySelector("body").childNodes[c].loc[1] -= camOffset[1]
-            refresh(document.querySelector("body").childNodes[c], 0, 0)
-        } catch {}
+            body.childNodes[c].loc[0] -= camOffset[0]
+            body.childNodes[c].loc[1] -= camOffset[1]
+            refresh(body.childNodes[c])
+        } catch { }
     }
 }
 setInterval(() => {
-    if(everyOther) fixed = fixeds[getClosest(moving)]
+    if (everyOther) fixed = fixeds[getClosest(moving)]
     everyOther = !everyOther;
-    collide(moving, fixed, 0.4)
-    if (!moving.clipping) move(moving, 1 / getDist(moving, fixed) * getAngle(moving, fixed)[0] * moving.weight, 1 / getDist(moving, fixed) * getAngle(moving, fixed)[1] * moving.weight)
+    collide(moving, fixed, 0.6)
+    angle = getAngle(moving, fixed);
+    if (!moving.clipping){
+        dist = getDist(moving, fixed);
+        move(moving, 1 / dist * angle[0], 1 / dist * angle[1])
+    }
     if (!controlMode) {
         if (keys["ArrowLeft"]) {
-            moving.speed[0] -= getAngle(moving, fixed)[1] * 0.01
-            moving.speed[1] += getAngle(moving, fixed)[0] * 0.01
+            moving.speed[0] -= angle[1] * 0.01
+            moving.speed[1] += angle[0] * 0.01
         }
         if (keys["ArrowRight"]) {
-            moving.speed[0] += getAngle(moving, fixed)[1] * 0.01
-            moving.speed[1] -= getAngle(moving, fixed)[0] * 0.01 }
-            if (keys["ArrowUp"]) {
-                moving.speed[0] -= getAngle(moving, fixed)[0] * 0.01
-                moving.speed[1] -= getAngle(moving, fixed)[1] * 0.01
-            }
-            if (keys["ArrowDown"]) {
-                moving.speed[0] += getAngle(moving, fixed)[0] * 0.05
-                moving.speed[1] += getAngle(moving, fixed)[1] * 0.05
-            }
+            moving.speed[0] += angle[1] * 0.01
+            moving.speed[1] -= angle[0] * 0.01
+        }
+        if (keys["ArrowUp"]) {
+            moving.speed[0] -= angle[0] * 0.01
+            moving.speed[1] -= angle[1] * 0.01
+        }
+        if (keys["ArrowDown"]) {
+            moving.speed[0] += angle[0] * 0.05
+            moving.speed[1] += angle[1] * 0.05
+        }
     } else {
         if (keys["ArrowLeft"]) {
             moving.speed[0] -= 0.01
@@ -108,12 +125,12 @@ setInterval(() => {
         }
     }
     render(keys["Control"] ? fixed : moving)
-    moving.style.background = controlMode ? "radial-gradient(#000, #FFF)" : `linear-gradient(${Math.atan2(getCenter(fixed)[1] - getCenter(moving)[1], getCenter(fixed)[0] - getCenter(moving)[0]) - Math.PI / 2 }rad, #FFF, #000)`
+    moving.style.background = controlMode ? "radial-gradient(#000, #FFF)" : `linear-gradient(${Math.atan2(getCenter(fixed)[1] - getCenter(moving)[1], getCenter(fixed)[0] - getCenter(moving)[0]) - Math.PI / 2}rad, #FFF, #000)`
 }, 0)
-        window.onkeydown = (e) => {
-            keys[e.key] = true
-        }
-        window.onkeyup = (e) => {
-            keys[e.key] = false
-            if (e.key == "Shift") controlMode = !controlMode
-        }
+window.onkeydown = (e) => {
+    keys[e.key] = true
+}
+window.onkeyup = (e) => {
+    keys[e.key] = false
+    if (e.key == "Shift") controlMode = !controlMode
+}
