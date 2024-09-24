25af683f56eb19a258ad4f09ed3ddfbbac512013
BurstRange <113287175+BrRange@users.noreply.github.com>
2024-09-24 16:40:05 -0300

    Moon rotation
    Moons now have a random start position and a clockwise/counter-clockwise rotation

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const body = document.querySelector("body");
 const keys = {};
+let objects = [];
 let controlMode = false;
 let everyOther = true;
-let fixedsNum = 10;
+let planetNum = 10;
 class Ball{
     constructor(element){
         this.element = element
         this.speed = [0, 0];
         this.loc = [offsetRNG(10000), offsetRNG(10000)];
+        objects[objects.length] = this;
     }
     getCenter(){
         return [this.loc[0] + this.radius, this.loc[1] + this.radius];
@@ -27,7 +29,11 @@ class Ball{
         let vector = this.getVector(target);
         return [vector[0] / mag, vector[1] / mag];
     }
-    refresh(){
+    render(target){
+        let focusCenter = target.getCenter();
+        let camOffset = [focusCenter[0] - window.innerWidth / 2, focusCenter[1] - window.innerHeight / 2];
+        this.loc[0] -= camOffset[0];
+        this.loc[1] -= camOffset[1];
         this.element.style.left = this.loc[0] + "px";
         this.element.style.top = this.loc[1] + "px";
     }
@@ -46,11 +52,14 @@ class Moon extends Ball{
         }
         body.appendChild(this.element);
         this.radius = this.element.offsetHeight / 2;
-        this.angularSpeed = Math.random() / 500;
-        this.deltaTime = 0//Math.random() * Math.PI * 2;
+        this.angularSpeed = Math.random() / 500 * (Math.random() > 0.5 ? 1 : -1);
+        this.deltaTime = Math.PI / 2//Math.random() * Math.PI * 2;
         this.anchorDist = (anchor.radius + this.radius) * (Math.random() + 1);
+        if(Math.random() < 0.2){
+            this.anchorDist *= 10;
+        }
         let anchorCenter = anchor.getCenter();
-        this.loc = [anchorCenter[0] - this.radius - this.anchorDist * Math.cos(this.deltaTime), anchorCenter[1] - this.radius + this.anchorDist * Math.sin(this.deltaTime)];
+        this.loc = [anchorCenter[0] - this.radius - this.anchorDist * Math.cos(this.deltaTime), anchorCenter[1] - this.radius - this.anchorDist * Math.sin(this.deltaTime)];
     }
     move(){
         this.deltaTime += this.angularSpeed;
@@ -61,6 +70,9 @@ class Moon extends Ball{
         this.loc[0] += this.speed[0];
         this.loc[1] += this.speed[1];
     }
+    tick(){
+        this.move();
+    }
 }
 class Fixed extends Ball{
     constructor(element){
@@ -70,12 +82,13 @@ class Fixed extends Ball{
         this.element.style = `background-color: rgb(${this.weight * diameter / 3}, ${255 - this.weight * 25}, ${this.weight + diameter}); width: ${diameter}vh; height: ${diameter}vh; border: #844 solid ${diameter / 5}pt; position: absolute; border-radius: 100%;`;
         body.appendChild(this.element);
         this.radius = this.element.offsetHeight / 2;
-        if(Math.random() > 0.2){
+        if(Math.random() < 1){
             this.moonSlot = new Moon(document.createElement("section"), this);
         } else {
             this.moonSlot = null;
         }
     }
+    tick(){}
 };
 class Moving extends Ball{
     constructor(){
@@ -115,94 +128,64 @@ class Moving extends Ball{
     }
     getClosest(){
         let close = [null, Infinity];
-        for (i in fixeds) {
-            let inCheck = fixeds[i];
+        for (i in objects) {
+            if(objects[i] == this) continue;
+            let inCheck = objects[i];
             let temp = this.getDist(inCheck) - inCheck.radius;
             close = close[1] > temp ? [inCheck, temp] : close;
-            if(inCheck.moonSlot != null){
-                inCheck = inCheck.moonSlot;
-                temp = this.getDist(inCheck) - inCheck.radius;
-                close = close[1] > temp ? [inCheck, temp] : close;
-            }
         }
         return close[0];
+    }
+    tick(){
+        this.move();
+        this.collide(closest);
     }
 };
 const moving = new Moving;
 function offsetRNG(limits) {
     return Math.random() * (limits * 2 + 1) - limits;
 }
-let fixeds = [];
-let fixed = null;
-for (i = 0; i < fixedsNum; i++) {
-    fixeds[i] = new Fixed(document.createElement("section"));
+let closest = null;
+for (i = 0; i < planetNum; i++) {
+    new Fixed(document.createElement("section"));
 }
-for(i in fixeds){
-    for(j = Number(i + 1); j < fixedsNum; j++){
-        let checkDist = fixeds[i].getDist(fixeds[j]);
-        if(checkDist / 2 < fixeds[i].radius + fixeds[j].radius){
-            let checkAngle = fixeds[i].getAngle(fixeds[j]);
-            fixeds[j].loc[0] += (fixeds[i].radius + fixeds[j].radius) * 2 * checkAngle[0];
-            fixeds[j].loc[1] += (fixeds[i].radius + fixeds[j].radius) * 2 * checkAngle[1];
+for(i in objects){
+    for(j = Number(i + 1); j < planetNum; j++){
+        if(!(objects[i] instanceof Fixed) || !(objects[j] instanceof Fixed)) continue;
+        let checkDist = objects[i].getDist(objects[j]);
+        if(checkDist / 2 < objects[i].radius + objects[j].radius){
+            let checkAngle = objects[i].getAngle(objects[j]);
+            objects[j].loc[0] += (objects[i].radius + objects[j].radius) * 2 * checkAngle[0];
+            objects[j].loc[1] += (objects[i].radius + objects[j].radius) * 2 * checkAngle[1];
         }
     }
 }
-function render(focusObj) {
-    let focusCenter = focusObj.getCenter();
-    let camOffset = [focusCenter[0] - window.innerWidth / 2, focusCenter[1] - window.innerHeight / 2];
-    moving.loc[0] -= camOffset[0];
-    moving.loc[1] -= camOffset[1];
-    moving.refresh();
-    for (i in fixeds) {
-        let inCheck = fixeds[i];
-        inCheck.loc[0] -= camOffset[0];
-        inCheck.loc[1] -= camOffset[1];
-        inCheck.refresh();
-        if(inCheck.moonSlot != null){
-            inCheck.moonSlot.move();
-            inCheck.moonSlot.loc[0] -= camOffset[0];
-            inCheck.moonSlot.loc[1] -= camOffset[1];
-            inCheck.moonSlot.refresh();
-        }
-    }
-}
-setInterval(() => {
-    if (everyOther) fixed = moving.getClosest();
+function mainLoop(){
+    if (everyOther) closest = moving.getClosest();
     everyOther = !everyOther;
-    moving.collide(fixed);
-    let angle = moving.getAngle(fixed);
+    let angle = moving.getAngle(closest);
     if (!moving.clipping && !everyOther){
-        for(i in fixeds){
-            let inCheck = fixeds[i];
+        for(i in objects){
+            if(objects[i] instanceof Moving) continue;
+            let inCheck = objects[i];
             let anglle = moving.getAngle(inCheck);
             let dist = moving.getDist(inCheck);
             let coef = inCheck.weight * inCheck.radius ** 2 / 100 / dist ** 2;
             moving.accelerate(coef * anglle[0], coef * anglle[1]);
-            if(inCheck.moonSlot != null){
-                inCheck = inCheck.moonSlot;
-                anglle = moving.getAngle(inCheck);
-                dist = moving.getDist(inCheck);
-                coef = inCheck.weight * inCheck.radius ** 2 / 100 / dist ** 2;
-                moving.accelerate(coef * anglle[0], coef * anglle[1]);
-            }
         }
     }
     if (!controlMode) {
         if (keys["ArrowLeft"]) {
-            moving.speed[0] -= angle[1] * 0.01;
-            moving.speed[1] += angle[0] * 0.01;
+            moving.accelerate(angle[1] * -0.01, angle[0] * 0.01);
         }
         if (keys["ArrowRight"]) {
-            moving.speed[0] += angle[1] * 0.01;
-            moving.speed[1] -= angle[0] * 0.01;
+            moving.accelerate(angle[1] * 0.01, angle[0] * -0.01);
         }
         if (keys["ArrowUp"]) {
-            moving.speed[0] -= angle[0] * (moving.grounded ? 0.1 : 0.02);
-            moving.speed[1] -= angle[1] * (moving.grounded ? 0.1 : 0.02);
+            moving.accelerate(angle[0] * -(moving.grounded ? 0.1 : 0.02), angle[1] * -(moving.grounded ? 0.1 : 0.02));
         }
         if (keys["ArrowDown"]) {
-            moving.speed[0] += angle[0] * 0.01;
-            moving.speed[1] += angle[1] * 0.01;
+            moving.accelerate(angle[0] * 0.01, angle[1] * 0.01);
         }
     } else {
         let direction = [0, 0];
@@ -220,15 +203,18 @@ setInterval(() => {
         }
         let norm = Math.sqrt(direction[0] ** 2 + direction[1] ** 2);
         if(norm == 0) norm = 1;
-        moving.speed[0] += 0.03 * direction[0] / norm;
-        moving.speed[1] += 0.03 * direction[1] / norm;
+        moving.accelerate(0.03 * direction[0] / norm, 0.03 * direction[1] / norm);
     }
-    moving.move();
-    render(keys["Control"] ? fixed : moving);
+    let focusObj = keys["Control"] ? closest : moving;
+    for(i in objects){
+        objects[i].render(focusObj);
+        objects[i].tick();
+    }
     let mCenter = moving.getCenter();
-    let fCenter = fixed.getCenter();
+    let fCenter = closest.getCenter();
     moving.element.style.background = controlMode ? "radial-gradient(#000, #FFF)" : `linear-gradient(${Math.atan2(fCenter[1] - mCenter[1], fCenter[0] - mCenter[0]) - Math.PI / 2}rad, #FFF, #000)`;
-}, 0);
+}
+setInterval(mainLoop, 0);
 window.onkeydown = (e) => {
     keys[e.key] = true;
 }
